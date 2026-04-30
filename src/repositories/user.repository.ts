2ee4fd3010b5e9b3ace0a8e7fbd777bf2a94b046
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-04-29 23:24:48 -0300

    criado o user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+export class UserRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  create = async (data: {
+    name: string
+    username: string
+    email: string
+    password: string
+    avatarUrl?: string
+  }) => {
+    return this.prisma.user.create({ data })
+  }
+
+  findByUsername = async (username: string) => {
+    return this.prisma.user.findUnique({ where: { username } })
+  }
+
+}
