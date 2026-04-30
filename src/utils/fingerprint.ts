ff52ecd376919e5d14f9fdfab7dd4eac3851f0d8
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-04-29 23:13:16 -0300

    feat: schema prisma

--- src/utils/fingerprint.ts
+++
@@ -1,8 +0,0 @@
-import { createHash } from 'crypto'
-import { Request } from 'express'
-
-export function generateFingerprint(req: Request): string {
-  const ip = req.ip ?? ''
-  const userAgent = req.headers['user-agent'] ?? ''
-  return createHash('sha256').update(ip + userAgent).digest('hex')
-}
