46ba70e8c69ba09efc667c889c71181b46e478c6
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 18:32:44 -0300

    feat: adicionado o follow no user router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { userController } from '../factories/user.factory'
+import { authMiddleware } from '../middlewares/auth.middleware'
 
 export const userRouter = Router()
 
 userRouter.post('/', userController.create)
-userRouter.get('/:username', userController.findByUsername)
+userRouter.get('/:username', authMiddleware, userController.findByUsername)
+userRouter.post('/:id/follow', authMiddleware, userController.follow)
+userRouter.delete('/:id/follow', authMiddleware, userController.unfollow)
