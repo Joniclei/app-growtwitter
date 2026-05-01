b2588832834e53b378e9ce58b53a13f69e34226e
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 19:19:38 -0300

    feat: importacao do validate e createUserSchema no router do user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { userController } from '../factories/user.factory'
 import { authMiddleware } from '../middlewares/auth.middleware'
+import { validate } from '../middlewares/validate.middleware'
+import { createUserSchema } from '../dtos/user.dto'
 
 export const userRouter = Router()
 
-userRouter.post('/', userController.create)
+userRouter.post('/', validate(createUserSchema), userController.create)
 userRouter.get('/:username', authMiddleware, userController.findByUsername)
 userRouter.post('/:id/follow', authMiddleware, userController.follow)
 userRouter.delete('/:id/follow', authMiddleware, userController.unfollow)
