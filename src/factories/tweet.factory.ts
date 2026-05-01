b557f90a24a3cdcd0238dc044343f5a38500a48a
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 14:44:45 -0300

    feat: adicionado o repository do like no factory do tweet

--- src/factories/tweet.factory.ts
+++ src/factories/tweet.factory.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { TweetRepository } from '../repositories/tweet.repository'
+import { LikeRepository } from '../repositories/like.repository'
 import { TweetService } from '../services/tweet.service'
 import { TweetController } from '../controllers/tweet.controller'
 
 const prisma = new PrismaClient()
 const tweetRepository = new TweetRepository(prisma)
-const tweetService = new TweetService(tweetRepository)
+const likeRepository = new LikeRepository(prisma)
+const tweetService = new TweetService(tweetRepository, likeRepository)
 export const tweetController = new TweetController(tweetService)
