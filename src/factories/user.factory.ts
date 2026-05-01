0513eddad56ccea3a2cdf89ee2b1dd39bda104ea
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 18:31:20 -0300

    feat: adicionar o followerrespository no service do factory

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 import { UserRepository } from '../repositories/user.repository'
+import { FollowerRepository } from '../repositories/follower.repository'
 import { UserService } from '../services/user.service'
 import { UserController } from '../controllers/user.controller'
 
 const prisma = new PrismaClient()
 const userRepository = new UserRepository(prisma)
-const userService = new UserService(userRepository)
+const followerRepository = new FollowerRepository(prisma)
+const userService = new UserService(userRepository, followerRepository)
 export const userController = new UserController(userService)
