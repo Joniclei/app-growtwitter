6f8b7945fe08b1e9185150011aaf811201e52e39
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 19:24:29 -0300

    feat: importacao dos schemas para tweet

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { tweetController } from '../factories/tweet.factory'
 import { authMiddleware } from '../middlewares/auth.middleware'
+import { validate } from '../middlewares/validate.middleware'
+import { createTweetSchema } from '../dtos/tweet.dto'
 
 export const tweetRouter = Router()
 
-tweetRouter.post('/', authMiddleware, tweetController.create)
-tweetRouter.post('/:id/reply', authMiddleware, tweetController.reply)
+tweetRouter.post('/', authMiddleware, validate(createTweetSchema), tweetController.create)
+tweetRouter.post('/:id/reply', authMiddleware, validate(createTweetSchema), tweetController.reply)
 tweetRouter.get('/feed', authMiddleware, tweetController.feed)
 tweetRouter.post('/:id/like', authMiddleware, tweetController.like)
 tweetRouter.delete('/:id/like', authMiddleware, tweetController.unlike)
