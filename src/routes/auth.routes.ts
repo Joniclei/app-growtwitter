89ededb5bcb8150b5763e970a1e6d1c8c27665d8
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-01 19:21:21 -0300

    fet: importacao da validacao para o login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { authController } from '../factories/auth.factory'
+import { validate } from '../middlewares/validate.middleware'
+import { loginSchema } from '../dtos/user.dto'
 
 export const authRouter = Router()
 
-authRouter.post('/login', authController.login)
+authRouter.post('/login', validate(loginSchema), authController.login)
