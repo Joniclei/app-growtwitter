a1d837abb94de35f9b5cdce4cc85c8cb8973d512
joniclei bernardo <Admin@jonicleis-MacBook-Pro.local>
2026-05-02 22:19:16 -0300

    adicionado a rota logout e adicionado nele o atuhmiddleware para guardar no blacklist

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { authController } from '../factories/auth.factory'
 import { validate } from '../middlewares/validate.middleware'
+import { authMiddleware } from '../middlewares/auth.middleware'
 import { loginSchema } from '../dtos/user.dto'
 
 export const authRouter = Router()
 
 authRouter.post('/login', validate(loginSchema), authController.login)
+authRouter.post('/logout', authMiddleware, authController.logout)
